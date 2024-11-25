8187b834a4c399a510e6fbc908fb84b80708fb11
alicemariaarrica <alicemariaarrica@gmail.com>
2024-11-25 15:36:56 +0100

    mainmenu.jsx

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+export default function Home() {
+    return (
+        <>
+
+            <h1>Home Page</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro, fugiat? Nisi, impedit magnam. Explicabo veniam, quibusdam dolorem quaerat maxime soluta minus voluptatibus exercitationem odit, voluptate fugiat, in dignissimos iure. Nesciunt.</p>
+
+
+        </>
+
+
+
+
+
+    )
+}
