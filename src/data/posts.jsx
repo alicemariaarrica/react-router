21e76080164fc3e02d13486a2f66771b468525e0
alicemariaarrica <alicemariaarrica@gmail.com>
2024-11-25 16:30:46 +0100

    posts.jsx-commit

---
+++ src/data/posts.jsx
@@ -0,0 +1,31 @@
+
+
+export default [{
+    name: 'post 1',
+    image: 'https://fastly.picsum.photos/id/315/200/200.jpg?hmac=cE5OEQSh9gvXqkP0fkrmaSbqLfc_KQdDPtH7yBbeuiQ',
+    content: 'lorem ipsum dolor',
+    category: [],
+    available: true
+},
+{
+    name: 'post 2',
+    image: 'https://fastly.picsum.photos/id/315/200/200.jpg?hmac=cE5OEQSh9gvXqkP0fkrmaSbqLfc_KQdDPtH7yBbeuiQ',
+    content: 'lorem ipsum dolor',
+    category: [],
+    available: true
+},
+{
+    name: 'post 3',
+    image: 'https://fastly.picsum.photos/id/315/200/200.jpg?hmac=cE5OEQSh9gvXqkP0fkrmaSbqLfc_KQdDPtH7yBbeuiQ',
+    content: 'lorem ipsum dolor',
+    category: [],
+    available: true
+},
+{
+    name: 'post 4',
+    image: 'https://fastly.picsum.photos/id/315/200/200.jpg?hmac=cE5OEQSh9gvXqkP0fkrmaSbqLfc_KQdDPtH7yBbeuiQ',
+    content: 'lorem ipsum dolor',
+    category: [],
+    available: true
+}
+];
